746741767226795fc8041f358045ca66f58542ac
James Parrish <javparrish@gmail.com>
2021-11-14 11:10:53 +0000

    Init

---
+++ AdventOfCode/Days/IDay.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Days
+{
+    public interface IDay
+    {
+        void Process();
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AdventOfCode.Days;
+
+namespace AdventOfCode
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var day = new Day3();
+            day.Process();
+        }
+    }
+}
